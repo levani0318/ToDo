12bfb5fd2e0d219976b7058ac3a6d0e31a9d85af
levani0318 <levani0318@gmail.com>
2025-04-18 20:41:40 +0400

    bg-right

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
-import { useState } from "react";
+import bgright from "./assets/bg-right.svg";
 
 function App() {
   return (
     <div className="h-screen flex justify-center items-center gap-[306px]">
       <h1 className="font-bold text-8xl text-[#007FDB]">ToDo</h1>
-      <div className="">
+      <div className="rounded-[10px]">
         <div className="">
-          <img src="" alt="" />
+          <img src={bgright} alt="bgright" />
         </div>
       </div>
     </div>
