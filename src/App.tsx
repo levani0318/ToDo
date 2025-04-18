97c9ecd373a466b0bfedd4200bac75b2a91e2a31
levani0318 <levani0318@gmail.com>
2025-04-18 20:28:35 +0400

    add font

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,16 @@
 import { useState } from "react";
 
 function App() {
-  return <></>;
+  return (
+    <div className="h-screen flex justify-center items-center gap-[306px]">
+      <h1 className="font-bold text-8xl text-[#007FDB]">ToDo</h1>
+      <div className="">
+        <div className="">
+          <img src="" alt="" />
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
