e302a6ea5aa04dc57a382a046c8fcc8af513be14
levani0318 <levani0318@gmail.com>
2025-04-20 23:25:50 +0400

    finish date

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,27 @@
 import bgright from "./assets/bg-right.svg";
 
 function App() {
-  const date = new Date();
-  const month = date.getMonth() + 1;
-  const year = date.getFullYear();
-  const today = date.getDate();
-  const options = {
-    timeZone: "America/New_York",
-    year: "numeric",
-    month: "long",
-    day: "numeric",
+  const currentTime = new Date().toLocaleString("us-en", {
     hour: "2-digit",
     minute: "2-digit",
-  };
-  const formattedDate = new Intl.DateTimeFormat("en-US", options).format(date);
-
+    hour12: true,
+  });
+  const currentDate = new Date().toLocaleString("en-us", {
+    weekday: "short",
+    day: "numeric",
+  });
+  console.log(currentDate.toLocaleString());
   return (
     <div className="h-screen flex justify-center items-center gap-[306px]">
       <h1 className="font-bold text-8xl text-[#007FDB]">ToDo</h1>
       <div className="rounded-[10px]">
-        <div className="">
-          <img src={bgright} alt="bgright" className="relative " />
-          <div className="absolute right-[345px] bottom-[294px]">
-            <h2>
-              {month}/{date}
+        <div className="relative">
+          <img src={bgright} alt="bgright" />
+          <div className="absolute right-[28px] bottom-[16px]">
+            <h2 className="font-normal text-[18px] text-white ml-[140px]">
+              {currentDate}
             </h2>
-            <h1>{formattedDate}</h1>
-            <h1></h1>
+            <h1 className="font-normal text-5xl text-white">{currentTime}</h1>
           </div>
         </div>
       </div>
